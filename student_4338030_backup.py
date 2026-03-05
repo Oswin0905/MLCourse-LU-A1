8ba8b41fda6bec2f4ea39a0590dae2f096339a15
oswin <o.e.t.van.den.bout@gmail.com>
2026-03-05 23:34:11 +0100

    Remove old notebook file

---
+++ student_4338030_backup.py
@@ -0,0 +1,10 @@
+############ CODE BLOCK 0 ################
+# ^ DO NOT CHANGE THIS LINE
+
+# You are not allowed to add additional imports!
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import sklearn
+from sklearn import datasets, ensemble, metrics, svm, model_selection, linear_model
+
